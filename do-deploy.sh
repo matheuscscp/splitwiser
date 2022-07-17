1a5fadde9f452598b2629e886bca0b94ff78ea54
Matheus Pimenta <matheuscscp@gmail.com>
2022-07-17 21:09:31 +0100

    add git tags on deploy

--- do-deploy.sh
+++ do-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 cur_tag=$(gcloud container images list-tags us-central1-docker.pkg.dev/splitwiser-356519/splitwiser/splitwiser --format=json | jq '.[0].tags[-1]' -r)
 
 echo "The current tag is $cur_tag."
@@ -16,3 +18,6 @@ docker build . -t $new_tag_fqn
 docker push $new_tag_fqn
 
 gcloud compute instances update-container splitwiser --container-image=$new_tag_fqn
+
+git tag $new_tag
+git push --tags
