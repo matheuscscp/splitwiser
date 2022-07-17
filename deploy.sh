c80db651b5845f2acf5f05f9f760ae0da9cc06cd
Matheus Pimenta <matheuscscp@gmail.com>
2022-07-17 21:03:09 +0100

    improve deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-cur_tag=$(gcloud container images list-tags us-central1-docker.pkg.dev/splitwiser-356519/splitwiser/splitwiser --format=json | jq '.[0].tags[-1]' -r)
-
-echo "The current tag is $cur_tag."
-
-cur_tag_number=$(echo $cur_tag | grep -oP '\d+')
-new_tag_number=$((cur_tag_number+1))
-new_tag="tag$new_tag_number"
-
-echo "The new tag is $new_tag."
-
-new_tag_fqn="us-central1-docker.pkg.dev/splitwiser-356519/splitwiser/splitwiser:$new_tag"
-
-docker build . -t $new_tag_fqn
-docker push $new_tag_fqn
-
-gcloud compute instances update-container splitwiser --container-image=$new_tag_fqn
+./do-deploy.sh > deploy-logs.txt 2>&1 &
