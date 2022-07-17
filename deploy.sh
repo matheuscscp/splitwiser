5300d5c1195be2af7e03f1d1fc6ead5d665c15a9
Matheus Pimenta <matheuscscp@gmail.com>
2022-07-17 22:26:23 +0100

    improve deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-./do-deploy.sh > deploy-logs.txt 2>&1 &
+git --no-pager diff --exit-code > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    ./do-deploy.sh > deploy-logs.txt 2>&1 &
+else
+    echo "Commit git changes first."
+fi
