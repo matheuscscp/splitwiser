1ff3ed56a1ee97dbb651ed80820f2d4d3e925541
Matheus Pimenta <matheuscscp@gmail.com>
2022-07-31 03:14:02 +0100

    multiple things
    * remove github actions
    
    * add missing permission
    
    * fix wrong secret volume mount paths

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git_status=$(git status --porcelain=v1 2>/dev/null | wc -l)
-if [ $git_status -eq 0 ]; then
-    ./do-deploy.sh > deploy-logs.txt 2>&1 &
-else
-    echo "Commit git changes first."
-fi
