4de99228689127199c993860dcacaedef72e67ba
Matheus Pimenta <matheuscscp@gmail.com>
2022-08-02 00:46:44 +0100

    add more apis to bootstrap

--- scripts/enable-bootstrap-googleapis.sh
+++ scripts/enable-bootstrap-googleapis.sh
@@ -3,3 +3,10 @@
 set -e
 
 gcloud services enable cloudresourcemanager.googleapis.com
+gcloud services enable secretmanager.googleapis.com
+gcloud services enable storage-api.googleapis.com
+gcloud services enable pubsub.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable cloudfunctions.googleapis.com
+gcloud services enable logging.googleapis.com
+gcloud services enable artifactregistry.googleapis.com
