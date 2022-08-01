726a490bfc4db91497a63e0a427f7ed4f333be9c
Matheus Pimenta <matheuscscp@gmail.com>
2022-08-01 13:45:35 +0100

    unify StartBot secrets service

--- cmd/startbot/main.go
+++ cmd/startbot/main.go
@@ -5,19 +5,13 @@ import (
 
 	_ "github.com/matheuscscp/splitwiser/cmd"
 	_ "github.com/matheuscscp/splitwiser/logging"
-	"github.com/matheuscscp/splitwiser/secrets"
 	"github.com/matheuscscp/splitwiser/startbot"
 
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	secretsService := secrets.NewMockService()
-	err := http.ListenAndServe("localhost:8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctxWithSecretsService := secrets.ContextWithService(r.Context(), secretsService)
-		reqWithContext := r.WithContext(ctxWithSecretsService)
-		startbot.Run(w, reqWithContext)
-	}))
+	err := http.ListenAndServe("localhost:8080", http.HandlerFunc(startbot.Run))
 	if err != nil {
 		logrus.Fatalf("error on ListenAndServe(): %v", err)
 	}
