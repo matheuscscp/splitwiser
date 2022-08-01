4b5e6cade648323cdf61cda48f76dd53cc92bfb5
Matheus Pimenta <matheuscscp@gmail.com>
2022-08-01 19:27:38 +0100

    add dev command for RotateSecret function

--- rotate_secret_function.go
+++ rotate_secret_function.go
@@ -2,15 +2,11 @@ package splitwiser
 
 import (
 	"context"
-	"fmt"
-	"hash/crc32"
 
 	_ "github.com/matheuscscp/splitwiser/logging"
-	"github.com/matheuscscp/splitwiser/secrets"
+	"github.com/matheuscscp/splitwiser/rotatesecret"
 
-	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	"github.com/sirupsen/logrus"
-	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 )
 
 // RotateSecret is a Pub/Sub Cloud Function.
@@ -20,53 +16,5 @@ func RotateSecret(ctx context.Context, m PubSubMessage) error {
 		return nil
 	}
 
-	// create client
-	client, err := secretmanager.NewClient(ctx)
-	if err != nil {
-		return fmt.Errorf("error creating secretmanager client: %w", err)
-	}
-	defer client.Close()
-
-	// find previous version
-	parent := m.Attributes.SecretId
-	latest, err := client.GetSecretVersion(ctx, &secretmanagerpb.GetSecretVersionRequest{
-		Name: fmt.Sprintf("%s/versions/latest", parent),
-	})
-	if err != nil {
-		logrus.Warnf("error fetching latest secret version: %v", err)
-		latest = nil
-	}
-
-	// generate secret and crc32 checksum
-	secret, err := secrets.Generate()
-	if err != nil {
-		return fmt.Errorf("error generating secret: %w", err)
-	}
-	secretPayload := []byte(secret)
-	checksum := int64(crc32.Checksum(secretPayload, crc32.MakeTable(crc32.Castagnoli)))
-
-	// add version
-	newVersion, err := client.AddSecretVersion(ctx, &secretmanagerpb.AddSecretVersionRequest{
-		Parent: parent,
-		Payload: &secretmanagerpb.SecretPayload{
-			Data:       secretPayload,
-			DataCrc32C: &checksum,
-		},
-	})
-	if err != nil {
-		return fmt.Errorf("error adding secret version: %w", err)
-	}
-
-	// destroy previous version
-	if latest != nil {
-		_, err = client.DestroySecretVersion(ctx, &secretmanagerpb.DestroySecretVersionRequest{
-			Name: latest.Name,
-		})
-		if err != nil {
-			return fmt.Errorf("error destroying previous secret version: %w", err)
-		}
-	}
-
-	logrus.Infof("secret rotated: %s", newVersion.Name)
-	return nil
+	return rotatesecret.Run(ctx, m.Attributes.SecretId)
 }
--- scripts/drop-branch.sh
+++ scripts/drop-branch.sh
@@ -3,15 +3,15 @@
 set -e
 
 git_status=$(git status --porcelain=v1 2>/dev/null | wc -l)
-if [ $git_status -eq 0 ]; then
-    ./do-deploy.sh > deploy-logs.txt 2>&1 &
-else
+if [ $git_status -ne 0 ]; then
+    git status
+    echo ""
     echo "Are you sure? You have uncommitted changes, consider using scripts/update-branch.sh."
     exit 1
 fi
 
 branch=$(git branch --show-current)
-git fall
+git fetch --prune --all --force --tags
 git update-ref refs/heads/main origin/main
 git checkout main
 git branch -D $branch
