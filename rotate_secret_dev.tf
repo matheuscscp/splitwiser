4e5551fea9952c456d2385a89dca1be092ca6646
Matheus Pimenta <matheuscscp@gmail.com>
2022-08-01 20:45:25 +0100

    add necessary permission for RotateSecret

--- rotate_secret_dev.tf
+++ rotate_secret_dev.tf
@@ -3,6 +3,12 @@ resource "google_service_account" "rotate-secret-dev" {
   display_name = "RotateSecret Development"
 }
 
+resource "google_project_iam_member" "rotate-secret-viewer-dev" {
+  project = local.project
+  member  = "serviceAccount:${google_service_account.rotate-secret-dev.email}"
+  role    = "roles/secretmanager.viewer"
+}
+
 resource "google_secret_manager_secret_iam_member" "rotate-secret-start-bot-jwt-secret-version-manager-dev" {
   secret_id = google_secret_manager_secret.start-bot-jwt-secret-dev.id
   member    = "serviceAccount:${google_service_account.rotate-secret-dev.email}"
