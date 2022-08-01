f7b0ab4751645456c3d295c32d6626b9faa2845b
Matheus Pimenta <matheuscscp@gmail.com>
2022-08-01 21:56:58 +0100

    add ctx to bot and checkpoint service

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+
 	_ "github.com/matheuscscp/splitwiser/cmd"
 	"github.com/matheuscscp/splitwiser/internal/bot"
 	_ "github.com/matheuscscp/splitwiser/logging"
@@ -9,7 +11,7 @@ import (
 )
 
 func main() {
-	if err := bot.Run(); err != nil {
+	if err := bot.Run(context.Background()); err != nil {
 		logrus.Fatalf("error running bot: %v", err)
 	}
 }
