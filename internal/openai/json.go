22322db67bd4f7a119315b731defdc951aaa66d3
Matheus Pimenta <matheuscscp@gmail.com>
2024-10-03 12:51:16 +0100

    fix clean openai json function

--- internal/openai/json.go
+++ internal/openai/json.go
@@ -1,18 +1,22 @@
 package openaipkg
 
-// CleanOpenAIJSONResponse cleans the JSON response from OpenAI API.
+// CleanOpenAIJSONResponse cleans the JSON response from OpenAI API
+// expecting it to be an array of objects.
+// In particular, this function is good for eliminating the enclosing
+// ```json{content}``` structure that OpenAI API returns, returning only
+// {content}.
 func CleanOpenAIJSONResponse(content string) string {
-	// eliminate everything before the first '{'
+	// eliminate everything before the first '['
 	for i, c := range content {
-		if c == '{' {
+		if c == '[' {
 			content = content[i:]
 			break
 		}
 	}
 
-	// eliminate everything after the last '}'
+	// eliminate everything after the last ']'
 	for i := len(content) - 1; i >= 0; i-- {
-		if content[i] == '}' {
+		if content[i] == ']' {
 			content = content[:i+1]
 			break
 		}
